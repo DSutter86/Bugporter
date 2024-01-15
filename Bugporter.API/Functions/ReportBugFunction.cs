c46fa8e92eeac50a85f9e5507da297af512aed69
DSutter86 <daniel.sutter@gmx.ch>
2024-01-15 20:54:59 +0100

    Add authentication

--- Bugporter.API/Functions/ReportBugFunction.cs
+++ Bugporter.API/Functions/ReportBugFunction.cs
@@ -10,24 +10,39 @@ using Newtonsoft.Json;
 using Bugporter.API.Features.ReportBug.GitHub;
 using Bugporter.API.Features.ReportBug;
 using Bugporter.API.Functions;
+using FirebaseAdminAuthentication.DependencyInjection.Services;
+using Microsoft.AspNetCore.Authentication;
+using FirebaseAdminAuthentication.DependencyInjection.Models;
 
 namespace Bugporter.API
 {
     public class ReportBugFunction
     {
         private readonly CreateGitHubIssueCommand _createGitHubIssueCommand;
+        private readonly FirebaseAuthenticationFunctionHandler _authenticationHandler;
         private readonly ILogger<ReportBugFunction> _logger;
 
-        public ReportBugFunction(ILogger<ReportBugFunction> logger, CreateGitHubIssueCommand createGitHubIssueCommand)
+        public ReportBugFunction(ILogger<ReportBugFunction> logger, FirebaseAuthenticationFunctionHandler authenticationFunctionHandler, CreateGitHubIssueCommand createGitHubIssueCommand)
         {
             _logger = logger;
+            _authenticationHandler = authenticationFunctionHandler;
             _createGitHubIssueCommand = createGitHubIssueCommand;
         }
 
         [FunctionName("ReportBugFunction")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "bugs")] ReportBugRequest request)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "bugs")] ReportBugRequest request, HttpRequest httpRequest)
         {
+            AuthenticateResult authenticationResult = await _authenticationHandler.HandleAuthenticateAsync(httpRequest);
+            
+            if (!authenticationResult.Succeeded)
+            {
+                return new UnauthorizedResult();
+            }
+
+            string userId = authenticationResult.Principal.FindFirst(FirebaseUserClaimType.ID).Value;
+            _logger.LogInformation("Authenticated user {userId}", userId);
+
             NewBug newBug = new NewBug(request.Summary, request.Description);
 
             ReportedBug reportedBug = await _createGitHubIssueCommand.Execute(newBug);
